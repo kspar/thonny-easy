8c96eaabd8bd264b1fde359dc1ec4e2700119ece
PriitPaluoja <priitpaluoja@gmail.com>
2021-01-08 13:36:45 +0200

    refactor

--- thonnycontrib/easy/easy_provider.py
+++ thonnycontrib/easy/easy_provider.py
@@ -9,10 +9,15 @@ from .ui import ExerciseProvider, FormData, EDITOR_CONTENT_NAME
 
 AUTH_ALLOWED_RETRIES = 6
 AUTH_TIMEOUT_SECONDS = 10
-ROOT = "/"
-HOME = [(ROOT, "Lahendus")]
+ROOT_PATH = "/"
+HOME = [(ROOT_PATH, "Lahendus")]
 LOGOUT_PATH = "/logout"
 AUTH_PATH = "/auth"
+
+EXERCISE_LIST_RE = re.compile(r"^/student/courses/[0-9]+/exercises/$")
+EXERCISE_DESCRIPTION_RE = re.compile(r"^/student/courses/[0-9]+/exercises/[0-9]+$")
+COURSE_LIST_RE = re.compile(r"^/student/courses$")
+SUBMIT_SOLUTION_RE = re.compile(r"^/student/courses/[0-9]+/exercises/[0-9]+/submissions$")
 
 
 def _get_easy():
@@ -29,18 +34,23 @@ class EasyExerciseProvider(ExerciseProvider):
 
     # Needed as separate function to avoid recursive call in auth handling
     def _handle_ui_request(self, url: str, form_data: FormData) -> Tuple[str, List[Tuple[str, str]]]:
-        if re.match(r"^/student/courses/[0-9]+/exercises/$", url):
+        if EXERCISE_LIST_RE.match(url):
             return self.show_exercise_list(url)
 
-        elif re.match(r"^/student/courses/[0-9]+/exercises/[0-9]+$", url):
+        elif EXERCISE_DESCRIPTION_RE.match(url):
             return self.show_exercise_description(url)
 
-        elif re.match(r"^/student/courses$", url) or url == ROOT:
+        elif COURSE_LIST_RE.match(url) or url == ROOT_PATH:
             return self.show_course_list()
 
-        elif re.match(r"^/student/courses/[0-9]+/exercises/[0-9]+/submissions$", url):
+        elif SUBMIT_SOLUTION_RE.match(url):
             return self.handle_submit_solution(form_data, url)
 
+        elif url == LOGOUT_PATH:
+            self.easy.logout_in_browser()
+            self.easy.shutdown()
+            self.easy = _get_easy()
+            return "<p>Nägemist!</p>", HOME
         else:
             return self.show_course_list()
 
@@ -64,14 +74,8 @@ class EasyExerciseProvider(ExerciseProvider):
                         self.easy.check_in()
 
                 form_url = form_data.get("from")
-                next_url = ROOT if form_url is None else form_url
+                next_url = ROOT_PATH if form_url is None else form_url
                 return self._handle_ui_request(next_url, form_data)
-
-            elif url == LOGOUT_PATH:
-                self.easy.logout_in_browser()
-                self.easy.shutdown()
-                self.easy = _get_easy()
-                return "<p>Nägemist!</p>", HOME
             else:
                 return self._handle_ui_request(url, form_data)
 
@@ -105,24 +109,23 @@ class EasyExerciseProvider(ExerciseProvider):
         course_id = url.replace("/student/courses/", "").replace("/exercises/", "")
         return self._get_ex_list(course_id)
 
-    def _get_course_list(self) -> Tuple[str, List[Tuple[str, str]]]:
-        courses = self.easy.student.get_courses().courses
-        return generate_course_list_html(courses), [self._breadcrumb_courses()]
+    def _get_course_list(self):
+        return generate_course_list_html(self.easy.student.get_courses().courses), [self._breadcrumb_courses()]
 
-    def _get_ex_list(self, course_id: str) -> Tuple[str, List[Tuple[str, str]]]:
+    def _get_ex_list(self, course_id: str):
         exercises = self.easy.student.get_course_exercises(course_id).exercises
         breadcrumb_ex_list = self._breadcrumb_exercises(course_id)
         html = generate_exercise_list_html(breadcrumb_ex_list[0], exercises)
 
         return html, [self._breadcrumb_courses(), breadcrumb_ex_list]
 
-    def _get_ex_description(self, course_id: str, exercise_id: str) -> Tuple[str, List[Tuple[str, str]]]:
+    def _get_ex_description(self, course_id: str, exercise_id: str):
         details = self.easy.student.get_exercise_details(course_id, exercise_id)
         breadcrumb_this = (f"/student/courses/{course_id}/exercises/{exercise_id}", details.effective_title)
         breadcrumbs = [self._breadcrumb_courses(), self._breadcrumb_exercises(course_id), breadcrumb_this]
         return generate_exercise_html(self, course_id, exercise_id), breadcrumbs
 
-    def _submit_solution(self, course_id: str, exercise_id: str, form_data) -> Tuple[str, List[Tuple[str, str]]]:
+    def _submit_solution(self, course_id: str, exercise_id: str, form_data):
         self.easy.student.post_submission(course_id, exercise_id, form_data.get(EDITOR_CONTENT_NAME))
         return self._get_ex_description(course_id, exercise_id)
 
