2b6ad4fb6939a939bf5bc8ddcd9385bdf1e4943f
PriitPaluoja <priitpaluoja@gmail.com>
2021-03-15 14:51:19 +0200

    update "updating required" message

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="thonny-lahendus",
-    version="1.0.0",
+    version="1.0.1",
     author="Priit Paluoja",
     author_email="priit.paluoja@gmail.com",
     description="Thonny plugin for lahendus.ut.ee",
