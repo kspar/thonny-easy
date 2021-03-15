6d1812bdf3512a2576d533155cc7fed4efbab330
PriitPaluoja <priitpaluoja@gmail.com>
2021-03-15 14:23:02 +0200

    fix code block going to the line end issue

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="thonny-lahendus",
-    version="0.6.7",
+    version="1.0.0",
     author="Priit Paluoja",
     author_email="priit.paluoja@gmail.com",
     description="Thonny plugin for lahendus.ut.ee",
