04e72c20b666403fd09b8ffa3bef25027112c976
PriitPaluoja <priitpaluoja@gmail.com>
2022-03-22 09:40:39 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="thonny-lahendus",
-    version="3.3.2",
+    version="3.3.3",
     author="Priit Paluoja",
     author_email="priit.paluoja@gmail.com",
     description="Thonny plugin for lahendus.ut.ee",
