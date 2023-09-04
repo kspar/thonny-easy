8d495d86c0849b9b9a3eb084c7bed7299d6debcf
PriitPaluoja <priitpaluoja@gmail.com>
2023-09-04 15:04:53 +0300

    EZ-1565 render OkV3 autoassessment feedback

--- thonnycontrib/easy/templates_generator.py
+++ thonnycontrib/easy/templates_generator.py
@@ -1,3 +1,4 @@
+import json
 import os
 from typing import Dict
 
@@ -27,7 +28,9 @@ def generate_exercise_list_html(base_url, exercises):
 
 
 def generate_course_list_html(courses):
-    course_lst = [f'<li><a href="/student/courses/{c["id"]}/exercises/">{c["title"] if c.get("alias", None) is None else c["alias"]}</a></li>' for c in courses]
+    course_lst = [
+        f'<li><a href="/student/courses/{c["id"]}/exercises/">{c["title"] if c.get("alias", None) is None else c["alias"]}</a></li>'
+        for c in courses]
     if len(course_lst) == 0:
         return "<div>Sind ei ole veel ühelegi kursusele lisatud.</div>"
     else:
@@ -56,6 +59,40 @@ def _convert_to_str(value):
     else:
         return str(value)
 
+def _status(status):
+    return status.replace('FAIL', '❌').replace('PASS', '✔')
+
+def _process_test(test):
+    try:
+        title, status = test['title'], _status(test['status'])
+        user_inputs, actual_output = test['user_inputs'], test['actual_output']
+
+        checks = [f"{(_status(check['status']))}:{check['feedback']}" for check in test["checks"]]
+        checks = "\n  ".join(checks)
+
+
+        if len(user_inputs) == 0:
+            user_inputs = ""
+        else:
+            user_inputs = "\n    ".join(user_inputs)
+            user_inputs =  f"  Andsin programmile sisendid: \n    {user_inputs}\n"
+
+        if actual_output is None:
+            actual_output = ""
+        else:
+            actual_output = f"  Programmi täielik väljund oli:\n    {actual_output}\n"
+
+        result = (
+            f"{status}:{title}\n"
+            f"  {checks}\n"
+            f"{user_inputs}"
+            f"{actual_output}"
+        )
+    except KeyError:
+        result = str(test)
+
+    return result
+
 
 def generate_exercise_html(provider, course_id, exercise_id) -> str:
     def has_submissions() -> bool:
@@ -66,11 +103,43 @@ def generate_exercise_html(provider, course_id, exercise_id) -> str:
     else:
         latest = SubmissionResp()
 
+    try:
+        js = json.loads(latest.feedback_auto)
+        if "result_type" in js:
+            result_type = js["result_type"]
+
+            if result_type == "OK_V3":
+                test_results = [_process_test(test) for test in js["tests"]]
+                feedback_auto = '\n'.join(test_results)
+                grade_auto = _convert_to_str(js["points"])
+
+            elif result_type == "OK_LEGACY":
+                feedback_auto = js["feedback"]
+                grade_auto = _convert_to_str(js["points"])
+
+            elif result_type == "ERROR_V3":
+                feedback_auto = js["error"]
+                grade_auto = "-"
+        else:
+            feedback_auto = latest.feedback_auto
+            grade_auto = _convert_to_str(latest.grade_auto)
+    except (ValueError, KeyError, TypeError):
+        feedback_auto = latest.feedback_auto
+        grade_auto = _convert_to_str(latest.grade_auto)
+
+    if _convert_to_str(latest.grade_teacher) is None:
+        points, feedback_type = (grade_auto, "⚙")
+    else:
+        points, feedback_type = (_convert_to_str(latest.grade_teacher), "🙎")
+
     details = provider.easy.student.get_exercise_details(course_id, exercise_id)
     return render("exercise.mustache", {"effective_title": details.effective_title,
                                         "text_html": details.text_html,
-                                        "grade_auto": _convert_to_str(latest.grade_auto),
-                                        "feedback_auto": latest.feedback_auto,
+                                        "is_open": details.is_open,
+                                        "not_open": not details.is_open,
+                                        "points": points,
+                                        "feedback_type": feedback_type,
+                                        "feedback_auto": feedback_auto,
                                         "solution": latest.solution,
                                         "EDITOR_CONTENT_NAME": EDITOR_CONTENT_NAME,
                                         "course_id": course_id,
