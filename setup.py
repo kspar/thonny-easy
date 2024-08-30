a7c285a4159f50dae859365aeafadc18e9359586
PriitPaluoja <priitpaluoja@gmail.com>
2024-08-30 15:07:49 +0300

    force update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="thonny-lahendus",
-    version="8.3.1",
+    version="9.0.0",
     author="Priit Paluoja",
     author_email="priit.paluoja@gmail.com",
     license="MIT",
